24f956ff57e46073bc3dd24e41983d67d08f34fd
Atadewa <aditafli42@gmail.com>
2025-02-18 16:01:28 +0700

    feat: update route to use greeting method in WelcomeController

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,4 @@ Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
 
-Route::get('/greeting', function(){
-    return view('blog.hello', ['name' => 'Aditya']);
-});
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
